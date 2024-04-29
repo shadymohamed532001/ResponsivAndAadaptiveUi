3f4983ca5f1bbd3c46a0131eaf59d01e79016f32
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-04-29 22:08:01 +0300

    Add AllExpencesItemHeaderModel class
    Add a new class AllExpencesItemHeaderModel with image, title, date, and price
    fields along with a constructor that initializes these fields.

---
+++ lib/models/all_expences_items_header_model.dart
@@ -0,0 +1,13 @@
+class AllExpencesItemHeaderModel {
+  final String image;
+  final String title;
+  final String date;
+  final String price;
+
+  AllExpencesItemHeaderModel({
+    required this.image,
+    required this.title,
+    required this.date,
+    required this.price,
+  });
+}
